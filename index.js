322d2f435b91c17a5c121df232d7d79b0ddb134e
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-03 18:11:08 +0530

    Update index.js
    added marvel name generator

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const superheroes = require('superheroes');
 superheroes.all;
 var myheroname =  superheroes.random();
 console.log(myheroname);
+//marvel name generator
+var names = require('marvel-comics-characters');
+var allNames = names.all;
+var randomName = names.random();
+var threeRandomNames = names.random(3);
+console.log(threeRandomNames);
